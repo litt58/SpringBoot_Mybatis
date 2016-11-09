9117e6e934c486c3f5f2fa995598ecc29a5bf866
李金钊 <li.jinzhao@jyall.com>
2016-11-09 17:20:23 +0800

    加入Redis作为缓存

--- src/main/java/com/jzli/conf/RedisCacheConfiguration.java
+++ src/main/java/com/jzli/conf/RedisCacheConfiguration.java
@@ -7,8 +7,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializer;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
  * =======================================================
@@ -52,10 +52,9 @@ public class RedisCacheConfiguration {
     public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory factory) {
         RedisTemplate<String, String> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(factory);
-
-        RedisSerializer redisSerializer = new MsgPackRedisSerializer();
-        redisTemplate.setKeySerializer(redisSerializer);
-        redisTemplate.setHashKeySerializer(redisSerializer);
+//        RedisSerializer msgPackRedisSerializer = new MsgPackRedisSerializer();
+//        redisTemplate.setKeySerializer(msgPackRedisSerializer);
+//        redisTemplate.setHashKeySerializer(msgPackRedisSerializer);
         return redisTemplate;
     }
 }
